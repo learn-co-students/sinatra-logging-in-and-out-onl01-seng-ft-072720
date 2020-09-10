23f377a54df70afb5092eda7df680ff0c8ee7c4a
ViolaCello <ViolaCelloNYC@gmail.com>
2020-09-10 13:08:30 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,23 @@
 class Helpers
+
+# def self.current_user(session)
+#     @user = User.all.find_by(id: session[:user_id])
+# end
+
+def self.current_user(session)
+    return unless session[:user_id]
+    current_user ||= User.find(session[:user_id])
+  end
+
+# def self.is_logged_in?(hash)
+#   #  binding.pry
+#   user = hash[:user_id]
+#     hash[:user_id] == user
+# end
+
+def self.is_logged_in?(session)
+ #   binding.pry
+    !session[:user_id].nil?
+  end
+
 end
